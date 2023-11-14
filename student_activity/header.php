7b66ae9acf13853f8030f6696ac1408b94bd8b0e
dpuPloy <65112462@dpu.ac.th>
2023-11-14 12:01:35 +0700

    profile.php

--- student_activity/header.php
+++ student_activity/header.php
@@ -13,6 +13,7 @@ require 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Student Activity</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   </head>
   <body>
   <div class="container">
@@ -28,7 +29,21 @@ require 'connect.php';
       </ul>
       <div class="dropdown text-end ms-3">
           <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+<?php
+$sql = "SELECT image FROM student WHERE studentID='{$_SESSION['user']['studentID']}'";
+$result = $conn->query($sql);
+$image = $result->fetch_assoc()['image'];
+if($image) {
+  echo "<img src='images/profile/$image'
+        class='rounded-circle' height='42'>";
+}
+else{
+  echo '<span class="material-symbols-outlined" 
+        style="font-size:42px"> face_3 </span>';
+}
+?>
+          <!-- // <span class="material-symbols-outlined"style="font-size:40px;">face_3</span>   -->
+          <!-- <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"> -->
           </a>
           <ul class="dropdown-menu text-small">
             <li><div class="dropdown-item text-primary"><?php echo $_SESSION['user']['studentName'];?></div></li>
